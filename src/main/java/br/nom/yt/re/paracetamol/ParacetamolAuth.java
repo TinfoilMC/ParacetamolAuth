d58430ff01ae7ef1fb94d4acb527c0cb3a03b6f8
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2026-01-19 11:27:03 -0300

    update for mc 1.21.11

--- src/main/java/br/nom/yt/re/paracetamol/ParacetamolAuth.java
+++ src/main/java/br/nom/yt/re/paracetamol/ParacetamolAuth.java
@@ -1,57 +1,63 @@
 package br.nom.yt.re.paracetamol;
 
+import com.mojang.authlib.exceptions.AuthenticationException;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConfirmScreen;
-import net.minecraft.client.util.NetworkUtils;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
 public class ParacetamolAuth implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		ClientLoginNetworking.registerGlobalReceiver(new Identifier("viaaas", "reauth"),
-				(client, handler, buf, listenerAdder) -> {
-					var username = buf.readString();
-					var connectionHash = buf.readString();
+    @Override
+    public void onInitializeClient() {
+        ClientLoginNetworking.registerGlobalReceiver(Identifier.of("oam", "join"),
+                (client, handler, buf, listenerAdder) -> {
+                    var connectionHash = buf.readString();
 
-					if (!username.equalsIgnoreCase(client.getSession().getUsername())) {
-						return CompletableFuture.completedFuture(null);
-					}
+                    var future = new CompletableFuture<PacketByteBuf>();
+                    client.execute(() -> {
+                        var oldScreen = client.currentScreen;
 
-					var future = new CompletableFuture<PacketByteBuf>();
-					client.execute(() -> {
-						var oldScreen = client.currentScreen;
+                        client.setScreen(new ConfirmScreen(
+                                result -> {
+                                    client.setScreen(oldScreen);
 
-						client.setScreen(new ConfirmScreen(
-								result -> {
-									client.setScreen(oldScreen);
+                                    if (result) {
+                                        try {
+                                            joinServer(client, connectionHash);
+                                            future.complete(new PacketByteBuf(generateSuccessResponse()));
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                            future.complete(null);
+                                        }
+                                    } else {
+                                        future.complete(null);
+                                    }
+                                },
+                                Text.translatable("viaaas.impersonate.title"),
+                                Text.translatable("viaaas.impersonate.content")
+                        ));
+                    });
 
-									if (result) {
-										NetworkUtils.EXECUTOR.execute(() -> {
-											try {
-												client.getSessionService().joinServer(client.getSession().getProfile(),
-														client.getSession().getAccessToken(), connectionHash);
-											} catch (Exception e) {
-												e.printStackTrace();
-												future.complete(null);
-											}
-											future.complete(PacketByteBufs.empty());
-										});
-									} else {
-										future.complete(null);
-									}
-								},
-								new TranslatableText("viaaas.impersonate.title"),
-								new TranslatableText("viaaas.impersonate.content")
-						));
-					});
+                    return future;
+                });
+    }
 
-					return future;
-				});
-	}
+    private void joinServer(MinecraftClient client, String connectionHash) throws AuthenticationException {
+        client.getApiServices().sessionService().joinServer(
+                client.getSession().getUuidOrNull(),
+                client.getSession().getAccessToken(),
+                connectionHash);
+    }
+
+
+    private ByteBuf generateSuccessResponse() {
+        return Unpooled.wrappedBuffer(new byte[]{1});
+    }
 }
